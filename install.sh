55c6dfc59b33a3c3bcb6c326203b61d7d31ba8d2
lainnie <l.diallo.pro@gmail.com>
2014-12-05 08:42:05 +0100

    -y to apt get install

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@ cd
 sudo apt-get install python-software-properties
 sudo add-apt-repository ppa:martin-frost/thoughtbot-rcm
 sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install curl git vim zsh rcm tmux
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install curl git vim zsh rcm tmux -y
 git clone https://github.com/lainnie/dotfiles.git
 curl -L http://install.ohmyz.sh | sh
 rm .zshrc

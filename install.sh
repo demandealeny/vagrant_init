277ecf6431b41ed9b78f1d0d9a4de8e8b134f882
Lainnie <l.diallo.pro@gmail.com>
2014-12-05 01:05:32 +0000

    Init commit

---
+++ install.sh
@@ -0,0 +1,12 @@
+cd
+sudo apt-get install python-software-properties
+sudo add-apt-repository ppa:martin-frost/thoughtbot-rcm
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install curl git vim zsh rcm tmux
+git clone https://github.com/lainnie/dotfiles.git
+curl -L http://install.ohmyz.sh | sh
+rm .zshrc
+rcup -v -d ~/dotfiles
+vim +PluginInstall +qall
+zsh
+chsh -s `which zsh`
